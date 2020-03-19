a275fda5a4d6e1f6ca05748b81dd888822e9db0b
Luis Rosado <luismrosado@yahoo.com>
2020-03-19 23:02:14 +0000

    medata and javascript string concatenation

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+// var language = 'JavaScript';
+// var status = 'Awesome';
+// var ourThoughts = language + ' is ' + status;
+
+// console.log(ourThoughts);
+
+// var greeting = 'Hello';
+// greeting = greeting + ' World!';
+
+// console.log('greeting:', greeting);
+
+// var capitalGreet = greeting.toUpperCase();
+// var lowerCaseGreet = greeting.toLowerCase();
+
+// console.log('Capital Greet:', capitalGreet);
+// console.log('lowerCaseGreet:', lowerCaseGreet);
+
+// var sentence = 'the quick brown fox jumped over the lazy dog';
+// var words = sentence.split(' ');
+// console.log(words);
+
+var firstName = 'Luis';
+var lastName = 'Rosado';
+
+console.log('firstName:', firstName);
+console.log('lastName:', lastName);
+
+var fullName = `
+Full Name
+
+---
+
+First Name: ${firstName}
+Last Name: ${lastName}`;
+
+console.log(fullName);
