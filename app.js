ed14ab49d6b0c9db4826a5d1b893778a3fdfed49
Luis Rosado <luismrosado@yahoo.com>
2020-03-20 04:24:08 +0000

    strings integers arrays

--- app.js
+++ app.js
@@ -21,18 +21,58 @@
 // var words = sentence.split(' ');
 // console.log(words);
 
-var firstName = 'Luis';
-var lastName = 'Rosado';
+// var firstName = 'Luis';
+// var lastName = 'Rosado';
 
-console.log('firstName:', firstName);
-console.log('lastName:', lastName);
+// console.log('firstName:', firstName);
+// console.log('lastName:', lastName);
 
-var fullName = `
-Full Name
+// var fullName = `
+// Full Name
 
----
+// ---
 
-First Name: ${firstName}
-Last Name: ${lastName}`;
+// First Name: ${firstName}
+// Last Name: ${lastName}`;
 
-console.log(fullName);
+// console.log(fullName);
+
+// var price = 23.09;
+// var tax = 0.07;
+// var total = price * (1 + tax);
+
+// console.log(total);
+
+//****************************************************** */
+//****************************************************** */
+// var answer = prompt('What time did you clock out?');* */
+//****************************************************** */
+//****************************************************** */
+// var solution = parseFloat(answer);
+
+// var number = solution + .30;
+
+// alert(number);
+
+// var age = 11;
+// age++;
+// console.log(age);
+// age--;
+// console.log(age);
+
+// Undefined is implicit emptiness
+// noVal is never given a value, so it is undefined
+// var noVal;
+// console.log(noVal); // undefined
+
+// Null is explicit emptiness
+// Values must be assigned Null
+// var nullVal = null;
+// console.log(nullVal);
+
+// var favoriteThings = ['dogs', 'nature', 'friends'];
+// var firstThing = favoriteThings[0];
+// var secondThing = favoriteThings[1];
+// var thirdThing = favoriteThings[2];
+
+// console.log(`I like ${firstThing}, ${secondThing}, and ${thirdThing}`);
