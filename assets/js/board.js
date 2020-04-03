f4e5202a0dc231c9ccda5b00285b1995256bd7c8
Luis Rosado <luismrosado@yahoo.com>
2020-04-03 16:27:21 +0000

    JavaScript class objects

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,8 +1,18 @@
 class Card {
     constructor(text) {
-        this.text = text;
+      this.text = text;
     }
+  }
+  
+class List {
+  constructor(title) {
+    this.title = title;
+    this.cards = [];
+  }
+
+  addCard(text) {
+    var card = new Card(text);
+    this.cards.push(card);
+  }
 }
 
-var card = new Card('My first card');
-console.log(card);
--- js/board.js
+++
@@ -1,8 +0,0 @@
-class Card {
-    constructor(text) {
-        this.text = text;
-    }
-}
-
-var card = new Card('My first card');
-console.log(card);
